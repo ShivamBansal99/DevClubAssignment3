5940efe8a74a3fbe5c8af4cb7395cf635ed67581
Shivam Bansal <cs1170376@iitd.ac.in>
2017-12-11 16:38:53 +0530

    Add design and some major Code Changes

--- app/src/main/java/com/shivam/test/MainActivity.java
+++ app/src/main/java/com/shivam/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.shivam.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int count=0;
     public void addcount(View view){
+        setContentView(R.layout.activity_main);
         TextView prev= (TextView) findViewById(R.id.textview);
-        int count= Integer.parseInt(prev.getText().toString());
         count=count+1;
+        String s=String.valueOf(count);
+        prev.setText(s);
+    }
+    public void resets(View view){
+        setContentView(R.layout.activity_main);
+        TextView prev= (TextView) findViewById(R.id.textview);
+        count=0;
         String s=String.valueOf(count);
         prev.setText(s);
     }
